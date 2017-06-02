354bf82f0088fcf4c68386f3da9f5a32795fa177
Raymond King <raymond.kingjr@gmail.com>
2017-06-01 22:20:46 -0700

    updates

--- contact.php
+++ contact.php
@@ -1,10 +1,10 @@
 <?php
 
-    $to = "your@email.com"; // ENTER EMAIL HERE
+    $to = "raymond.kingjr@gmail.com"; // ENTER EMAIL HERE
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $headers = "From: $from";
-    $subject = "You have a message.";
+    $subject = "You have a message from SGT Code Web.";
 
     $fields = array();
     $fields{"name"} = "name";
